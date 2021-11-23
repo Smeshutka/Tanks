9499ba56d683d04cc57ec8fbad1c5b3b45bb79dd
Smeshutka <m.s.sivak@mail.ru>
2021-11-23 18:59:52 +0300

    wrote funcs move and draw

--- objects.py
+++ objects.py
@@ -2,7 +2,18 @@ import pygame
 import math
 
 dt = 1/60
-d_ang = 0.01
+
+
+def update(image, w, h, angle):
+    """
+    Меняет исходные размеры картинки, а затем поворачивает ее
+    w, h: новые размеры картинки
+    angle: угол поворота картинки
+    """
+
+    image = pygame.transform.scale(image, (w, h))
+    image = pygame.transform.rotate(image, angle)
+    
 class pos():
     def __init__(self, x, y):
         self.x = x
@@ -13,6 +24,7 @@ class Tank():
         """
         args: x,y - положение центра танка
         tank_type - тип танка, возможные: "light", "middle", "heavy"
+        sreen - экран на котором будет отображаться танквввввв
         """
         self.screen = screen
         self.center = pos(x,y)
@@ -28,6 +40,7 @@ class Tank():
             self.body_image = pygame.image.load("textures/light_body.png").convert_alpha()
             self.turret_image = pygame.image.load("textures/light_turret.png").convert_alpha()
             self.engine_power = 3
+            self.ang_speed = 2*math.pi/10
         
         elif tank_type == "middle":
             self.hp = 5
@@ -35,6 +48,7 @@ class Tank():
             self.body_image = pygame.image.load("textures/middle_body.png").convert_alpha()
             self.turret_image = pygame.image.load("textures/middle_turret.png").convert_alpha()
             self.engine_power = 2
+            self.ang_speed = 2*math.pi/10
         
         elif tank_type == "heavy":
             self.hp = 5
@@ -42,6 +56,7 @@ class Tank():
             self.body_image = pygame.image.load("textures/heavy_body.png").convert_alpha()
             self.turret_image = pygame.image.load("textures/heavy_turret.png").convert_alpha()
             self.engine_power = 100
+            self.ang_speed = 2*math.pi/10
         
         self.corner = pos(self.center.x-self.size.x/2, self.center.y-self.size.y/2)
         
@@ -54,23 +69,27 @@ class Tank():
         кнопка под второй буквой переменной, False если нет
         """
         global dt
+        
         def turn(self, fa, fd):
             if fa == True and fd == False:
-                self.body_ang += d_ang
+                self.body_ang += self.ang_speed * dt
             elif fa == False and fd == True:
-                self.body_ang -= d_ang
+                self.body_ang -= self.ang_speed * dt
+        
         def turn_turret(self):
             mx , my = pygame.mouse.get_pos()
             if mx > self.center.x:
                 self.turret_ang = -math.atan((my-self.center.y) / (mx-self.center.x))
             elif mx < self.center.x:
                 self.turret_ang = -math.atan((my-self.center.y) / (mx-self.center.x))+math.pi
+        
         def set_acceleration(self, fw, fs, k1, k2):
             self.acceleration.x = 0
             self.acceleration.y = 0
             an = self.body_ang
             vx = self.velocity.x
             vy = self.velocity.y
+            print(vx, vy)
             v = (vx**2 + vy**2)**0.5
             vel_ang = 0
             if vx > 0:
@@ -102,6 +121,9 @@ class Tank():
             
     def draw(self):
         image = pygame.transform.rotate(self.body_image, self.body_ang*180/math.pi)
+        a, b = image.get_size()
+        self.screen.blit(image, (self.center.x-a/2, self.center.y-b/2))
+        image = pygame.transform.rotate(self.turret_image, self.turret_ang*180/math.pi)
         a, b = image.get_size()
         self.screen.blit(image, (self.center.x-a/2, self.center.y-b/2))
             
@@ -142,5 +164,5 @@ while not finished:
             if event.key == pygame.K_d:
                 fd = 0
     
-    tank.move(fw,fa,fs,fd,0.01,100)
+    tank.move(fw,fa,fs,fd,0.01,1)
 pygame.quit()
