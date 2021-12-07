dadb498e054ba28048eb8a93e92ad545c3e7c044
1MDaniil1 <gfhjkmub[f,1b>
2021-12-07 19:40:58 +0300

    changes

--- map_maker/tiles.py
+++ map_maker/tiles.py
@@ -2,6 +2,8 @@ import os
 import sys
 
 #sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import pygame
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..\..')))
 from Tanks.helper import*
 from Tanks.constans import*
