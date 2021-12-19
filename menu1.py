f342f09d77d7d431854eaf53d132caba64ba9852
1MDaniil1 <90687735+1MDaniil1@users.noreply.github.com>
2021-12-19 18:27:15 +0300

    Add files via upload

--- menu1.py
+++ menu1.py
@@ -1,4 +1,3 @@
-import sys
 from button_class import *
 import game
 import level_constructor
@@ -8,7 +7,10 @@ import server
 import client
 from tank_class import *
 
-'''Функции окон меню'''
+
+pygame.mixer.init()
+
+
 
 
 def menu_main():
@@ -50,11 +52,6 @@ def menu_main():
 
 
 def menu_singleplayer():
-    def start_function():
-        ending = game.game_main(game_input, tank_type)
-        if ending != None:
-            eval(ending)
-
     pygame.init()
 
     global game_input
@@ -118,128 +115,12 @@ def menu_multiplayer():
     button1 = Button(screen, 10, 10, 150, 50, 'go_back')
     button2 = Button(screen, 250, 100, 300, 100, 'host_game')
     button3 = Button(screen, 250, 220, 300, 100, 'join_game')
-
-    buttons = [button1, button2, button3]
-
-    while not finished:
-        screen.fill((0, 0, 0))
-
-        menu.draw()
-        for button in buttons:
-            button.check_pressed()
-            button.draw()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                finished = True
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    for button in buttons:
-                        if button.pressed:
-                            eval(button.trigger())
-        pygame.display.update()
-        clock.tick(FPS)
-
-
-def menu_multiplayer_host():
-    def start_function():
-        server.server_main(ip_entry.text, port_entry.text, game_input)
-
-    time_meazure = 0
-    pygame.init()
-
-    global game_input
-    game_input = "map_maker/maps/1.txt"
-
-    screen = pygame.display.set_mode((w, h))
-    clock = pygame.time.Clock()
-
-    finished = False
-
-    menu = Static(screen, 0, 0, w, h, 'metall')
-    button1 = Button(screen, 10, 10, 150, 50, 'go_back')
-    button2 = Button(screen, 100, 250, 200, 66, 'choose_level')
-    button3 = Button(screen, 250, 490, 300, 100, 'start_game')
-    ip_entry = Entry(screen, 100, 400, 200, 50)
-    port_entry = Entry(screen, 500, 400, 200, 50)
-    players_entry = Entry(screen, 500, 250, 200, 50)
-    level_text = Entry(screen, 75, 150, 250, 66, game_input)
-    ip_text = Entry(screen, 100, 350, 200, 50, 'IP', None)
-    port_text = Entry(screen, 500, 350, 200, 50, 'port', None)
-    players_text = Entry(screen, 500, 200, 200, 50, 'amount of players', None)
-
-    buttons = [button1, button2, button3]
-    entries = [ip_entry, port_entry, players_entry]
-    text = [ip_text, port_text, players_text]
-
-    while not finished:
-        screen.fill((0, 0, 0))
-
-        menu.draw()
-        for button in buttons:
-            button.check_pressed()
-            button.draw()
-
-        for entry in entries:
-            entry.check_pressed()
-            if (time_meazure // 1 % 2) == 0:
-                entry.draw(True)
-            else:
-                entry.draw()
-
-        for t in text:
-            t.draw()
-
-        level_text.change_text(game_input)
-        level_text.draw()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                finished = True
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    for button in buttons:
-                        if button.pressed:
-                            eval(button.trigger())
-                    for entry in entries:
-                        entry.trigger()
-            if event.type == pygame.KEYUP:
-                ip_entry.writing(event, True)
-                port_entry.writing(event)
-
-        pygame.display.update()
-
-        time_meazure += 2 / FPS
-        clock.tick(FPS)
-
-
-def menu_multiplayer_join():
-    def start_function():
-        client.client_main(ip_entry.text, port_entry.text)
-
-    time_meazure = 0
-    pygame.init()
-
-    global tank_type
-    tank_type = 'light'
-
-    screen = pygame.display.set_mode((w, h))
-    clock = pygame.time.Clock()
-
-    finished = False
-
-    menu = Static(screen, 0, 0, w, h, 'metall')
-    button1 = Button(screen, 10, 10, 150, 50, 'go_back')
-    button2 = Button(screen, 250, 490, 300, 100, 'start_game')
-    tank1 = Button(screen, 220, 150, 100, 200, 'tank_light')
-    tank2 = Button(screen, 370, 150, 100, 200, 'tank_middle')
-    tank3 = Button(screen, 520, 150, 119, 200, 'tank_heavy')
     ip_entry = Entry(screen, 100, 400, 200, 50)
     port_entry = Entry(screen, 500, 400, 200, 50)
     ip_text = Entry(screen, 100, 350, 200, 50, 'IP', None)
     port_text = Entry(screen, 500, 350, 200, 50, 'port', None)
 
-    buttons = [button1, button2, tank1, tank2, tank3]
+    buttons = [button1, button2, button3]
     entries = [ip_entry, port_entry]
     text = [ip_text, port_text]
 
@@ -268,10 +149,7 @@ def menu_multiplayer_join():
                 if event.button == 1:
                     for button in buttons:
                         if button.pressed:
-                            button.set_bg_color((255, 255, 0))
                             eval(button.trigger())
-                        else:
-                            button.set_bg_color(None)
                     for entry in entries:
                         entry.trigger()
             if event.type == pygame.KEYUP:
@@ -293,8 +171,8 @@ def menu_settings():
     finished = False
 
     menu = Static(screen, 0, 0, w, h, 'metall')
-    button1 = Button(screen, 10, 10, 150, 50, 'go_back')
-    button2 = Button(screen, 10, 80, 150, 50, 'music')
+    button1 = Button(screen, 300, 100, 250, 100,  'go_back')
+    button2 = Button(screen, 300, 220, 250, 100,  'music')
     buttons = [button1, button2]
 
     while not finished:
@@ -317,113 +195,6 @@ def menu_settings():
         pygame.display.update()
 
         clock.tick(FPS)
-
-
-def music():
-    pygame.init()
-
-    screen = pygame.display.set_mode((w, h))
-    clock = pygame.time.Clock()
-
-    finished = False
-
-    menu = Static(screen, 0, 0, w, h, 'metall')
-    button1 = Button(screen, 350, 100, 150, 50, 'on')
-    button2 = Button(screen, 350, 170, 150, 50, 'off')
-    buttons = [button1, button2]
-
-    while not finished:
-        screen.fill((0, 0, 0))
-
-        menu.draw()
-        for button in buttons:
-            button.check_pressed()
-            button.draw()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                finished = True
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    for button in buttons:
-                        if button.pressed:
-                            eval(button.trigger())
-
-        pygame.display.update()
-
-        clock.tick(FPS)
-
-
-def death():
-    pygame.init()
-
-    screen = pygame.display.set_mode((w, h))
-    clock = pygame.time.Clock()
-
-    finished = False
-
-    menu = Static(screen, 0, 0, w, h, 'floppa')
-    button1 = Button(screen, 10, 10, 150, 50, 'go_back')
-    buttons = []
-
-    while not finished:
-        screen.fill((0, 0, 0))
-
-        menu.draw()
-        for button in buttons:
-            button.check_pressed()
-            button.draw()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                finished = True
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    for button in buttons:
-                        if button.pressed:
-                            eval(button.trigger())
-
-        pygame.display.update()
-
-        clock.tick(FPS)
-
-
-def win():
-    pygame.init()
-
-    screen = pygame.display.set_mode((w, h))
-    clock = pygame.time.Clock()
-
-    finished = False
-
-    menu = Static(screen, 0, 0, w, h, 'floppa_win')
-    button1 = Button(screen, 10, 10, 150, 50, 'go_back')
-    buttons = []
-
-    while not finished:
-        screen.fill((0, 0, 0))
-
-        menu.draw()
-        for button in buttons:
-            button.check_pressed()
-            button.draw()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                finished = True
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    for button in buttons:
-                        if button.pressed:
-                            eval(button.trigger())
-
-        pygame.display.update()
-
-        clock.tick(FPS)
-
-
-
-'''Вспомогательные функции'''
 
 
 def choose_level():
@@ -445,12 +216,70 @@ def set_tank_type(type):
     tank_type = type
 
 
+def music():
+    pygame.init()
+
+    screen = pygame.display.set_mode((w, h))
+    clock = pygame.time.Clock()
+
+    finished = False
+
+    menu = Static(screen, 0, 0, w, h, 'metall')
+    button1 = Button(screen, 270, 100, 250, 100, 'go_back')
+    button2 = Button(screen, 140, 250, 250, 100, 'on')
+    button3 = Button(screen, 400, 250, 250, 100, 'off')
+
+
+    buttons = [button1, button2, button3]
+
+    for i in range(5):
+        buttons.append(Button(screen, 140+i*105, 400, 90, 50, str(i+1)))
+
+
+    while not finished:
+        screen.fill((0, 0, 0))
+
+        menu.draw()
+        for button in buttons:
+            button.check_pressed()
+            button.draw()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    for button in buttons:
+                        if button.pressed:
+                            eval(button.trigger())
+
+        pygame.display.update()
+
+        clock.tick(FPS)
+
 def on():
     pygame.mixer.music.load("music/1.mp3")
+    pygame.mixer.music.set_volume(0.7)
     pygame.mixer.music.play()
-
-
 def off():
     pygame.mixer.music.pause()
+
+def one():
+    pygame.mixer.music.set_volume(0.19*2)
+
+def two():
+    pygame.mixer.music.set_volume(0.19*4)
+
+def three():
+    pygame.mixer.music.set_volume(0.19*6)
+
+def four():
+    pygame.mixer.music.set_volume(0.19*8)
+
+def five():
+    pygame.mixer.music.set_volume(0.19*10)
+
+def
+
 
 menu_main()
